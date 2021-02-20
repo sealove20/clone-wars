9afc4a024626bc0623eb227ce8a326aa341de938
Matheus Henrique <matheus.181137@gmail.com>
2021-02-19 20:14:52 -0400

    refactor: change next and previous buttons paginator icons

--- src/components/Paginator/index.tsx
+++ src/components/Paginator/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import ReactPaginate from 'react-paginate'
 
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
 type Props = {
   pageCount: number
   handlePageClick: (data: number) => void
@@ -9,8 +11,8 @@ type Props = {
 const Paginator: React.FunctionComponent<Props> = ({ pageCount, handlePageClick }: Props) => {
   return (
     <ReactPaginate
-      previousLabel={'previous'}
-      nextLabel={'next'}
+      previousLabel={<FiChevronLeft className="w-7 h-7" />}
+      nextLabel={<FiChevronRight className="w-7 h-7" />}
       breakLabel={'...'}
       breakClassName={'break-me'}
       pageCount={pageCount}
@@ -19,6 +21,7 @@ const Paginator: React.FunctionComponent<Props> = ({ pageCount, handlePageClick 
       onPageChange={(data) => handlePageClick(data.selected)}
       containerClassName={'flex list-none p-0'}
       pageClassName={'mx-4'}
+      pageLinkClassName={'outline-none'}
       activeClassName={'bg-blue-300 dark:bg-blue-500 rounded-2xl h-6 w-6 flex justify-center'}
       initialPage={0}
     />
