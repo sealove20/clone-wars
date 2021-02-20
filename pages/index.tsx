7dc32593437195a458d5b8da7aae5643f7475a09
Matheus Henrique <matheus.181137@gmail.com>
2021-02-19 20:24:46 -0400

    fix: move card contet from card header

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,15 +74,15 @@ const Home: React.FunctionComponent = () => {
         <div key={character.name} className="h-72 w-72 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-lg rounded-md my-5 hover:scale-50">
           <div className="flex justify-center bg-yellow-200 dark:bg-yellow-700 rounded-t-md shadow-lg">
             <h2 className="text-2xl text-center h-20">{character.name}</h2>
-            <div className="flex flex-col justify-start items-center m-4">
-              <p className="text-gray-200">Birth: {character.birth_year}</p>
-              <p className="text-gray-200">Eye Color: {character.eye_color}</p>
-              <p className="text-gray-200">Gender: {character.gender}</p>
-              <p className="text-gray-200">Hair Color: {character.hair_color}</p>
-              <p className="text-gray-200">Height: {character.height}</p>
-              <p className="text-gray-200">Mass: {character.mass}</p>
-              <p className="text-gray-200">Skin Color: {character.skin_color}</p>
-            </div>
+          </div>
+          <div className="flex flex-col justify-start items-center m-4">
+            <p className="text-gray-200">Birth: {character.birth_year}</p>
+            <p className="text-gray-200">Eye Color: {character.eye_color}</p>
+            <p className="text-gray-200">Gender: {character.gender}</p>
+            <p className="text-gray-200">Hair Color: {character.hair_color}</p>
+            <p className="text-gray-200">Height: {character.height}</p>
+            <p className="text-gray-200">Mass: {character.mass}</p>
+            <p className="text-gray-200">Skin Color: {character.skin_color}</p>
           </div>
         </div>
         ))}
