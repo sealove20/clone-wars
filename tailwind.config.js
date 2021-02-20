f0b027709442f0318dd84510eacf1c7ef76e7d60
Matheus Henrique <matheus.181137@gmail.com>
2021-02-19 21:09:42 -0400

    chore: change tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    screens: {
+      sm: '590px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
     extend: {}
   },
   variants: {
